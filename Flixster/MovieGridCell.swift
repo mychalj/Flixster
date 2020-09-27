6f2a16a66a415b366467f675a0d2552b3fc4b42a
Mychal Johnson <mychalqj@gmail.com>
2020-09-27 13:18:19 -0400

    Movies added to superhero tab

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Mychal Johnson on 9/27/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
